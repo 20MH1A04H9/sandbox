bef690d4f3e74d235e9f8307fbac2e8e4e52c3ba
VISWA <109515505+20MH1A04H9@users.noreply.github.com>
2024-05-25 08:59:02 +0530

    Create Num.py

---
+++ Num.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a NumPy array from a list of numbers
+numbers = [1, 2, 3, 4, 5]
+numbers_array = np.array(numbers)
+
+# Print the array
+print(numbers_array)
+
+
+# output: [1 2 3 4 5]
