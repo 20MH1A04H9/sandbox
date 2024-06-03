ed04856b1a031640e0fece671090a3a25454a5b4
VISWA <109515505+20MH1A04H9@users.noreply.github.com>
2024-06-03 21:18:37 +0530

    Create broadcas.py

---
+++ broadcas.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Create a two-dimensional NumPy array
+x = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+# Add a scalar value to each element of the array
+y = x + 10
+
+# Print the resulting array
+print(y)
+
