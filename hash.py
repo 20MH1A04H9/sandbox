d0e4641d940db023d74115efc9f229276a909fef
VISWA <109515505+20MH1A04H9@users.noreply.github.com>
2024-06-03 21:20:39 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,2 +1,6 @@
-my_object = "Hello, world!"
-my_hash = hash(my_object)
+my_object1 = "Hello, world!"
+my_object2 = (1, 2, 3)
+my_object3 = {"key": "value"}
+my_hash1 = hash(my_object1)
+my_hash2 = hash(my_object2)
+my_hash3 = hash(my_object3)
